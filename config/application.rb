400484bbb56272ce2ff5296ec4d73424ce069a21
coco <koko_16N1300059@nnn.ed.jp>
2016-10-21 07:41:09 +0900

    heroku助けて

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,9 @@ module Taskapp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.assets.initialize_on_precompile = false
+    
   end
 end
 
 # For Heroku
-    config.assets.initialize_on_precompile = false
